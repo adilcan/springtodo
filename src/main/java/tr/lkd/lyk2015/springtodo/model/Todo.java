4992bf8683eb1b14c669d84187f50c43e8b9004b
adilcan <adilcaneren@gmail.com>
2015-08-17 11:20:26 +0300

    springtodo

---
+++ src/main/java/tr/lkd/lyk2015/springtodo/model/Todo.java
@@ -0,0 +1,79 @@
+package tr.lkd.lyk2015.springtodo.model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.Table;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+@Entity
+
+public class Todo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@NotEmpty
+	private String name;
+	
+	@Size(max = 10)
+	@Column(name = "description")
+	private String desc;
+
+	@DateTimeFormat(iso = ISO.DATE)
+	private Calendar dueDate;
+
+	private boolean done;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public Calendar getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Calendar dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+}
